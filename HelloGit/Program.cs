fa2b257c2a567d2d9e38fb073ebce26fc91d6406
ironma <ironma@gmail.com>
2015-01-20 23:30:35 +0200

    Console App Created

---
+++ HelloGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloGit
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Zdrasti GIT-eeeeeeeeeeeeee!");
+        }
+    }
+}
